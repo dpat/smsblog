a9b208132e6163daa6550d048e40894eb636e994
dpat <david.patawaran@gmail.com>
2018-08-23 15:02:23 -0600

    better formatting

--- smsblog/helpers/formatting.py
+++ smsblog/helpers/formatting.py
@@ -3,5 +3,5 @@ import json
 
 def response_string(response):
     status_code = str(response.status_code)
-    text = json.dumps((response.data).decode('utf-8'))
-    return status_code + ' ' + text
+    text = json.loads((response.data).decode('utf-8'))
+    return status_code + ' ' + str(text)
