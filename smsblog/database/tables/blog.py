7deebe4618c04e6dd1e1ccd81a9fddcc3ae12e86
David Patawaran <dpatawaran@carbonblack.com>
2018-11-28 16:02:15 -0700

    forgot column

--- smsblog/database/tables/blog.py
+++ smsblog/database/tables/blog.py
@@ -16,5 +16,6 @@ class Blog(DB.Model):
     date = DB.Column(DB.DateTime, nullable=False,
                      default=datetime.datetime.utcnow)
     postid = DB.Column(DB.Integer, nullable=False, primary_key=True)
+    title = DB.Column(DB.Text, nullable=False)
     category = DB.Column(DB.Text, nullable=False)
     post = DB.Column(DB.Text, nullable=False)
