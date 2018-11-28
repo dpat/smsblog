be3a770105da633f47a57e60a2fa2574b09deb43
David Patawaran <dpatawaran@carbonblack.com>
2018-11-28 13:02:35 -0700

    might work

--- smsblog/routes/blog.py
+++ smsblog/routes/blog.py
@@ -79,7 +79,7 @@ def update_post(id, category, post):
     """Update a post based on its post id."""
     post_id = int(id)
     new_post = ' '.join(post)
-    old_post = query_postid(post_id)
+    old_post = DB.session.query(Blog).get(post_id)
     setattr(old_post, 'post', 'test')
     DB.session.commit()
 
